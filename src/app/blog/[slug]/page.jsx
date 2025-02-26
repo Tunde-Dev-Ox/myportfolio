a1bdf554fd2a98abc8b6c3a736e5dfcd9b751092
Tunde <iamjosephtunde@gmail.com>
2025-02-26 16:47:31 +0100

    ........

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -264,6 +264,18 @@
 
 
 // app/blog/[slug]/page.js (Server Component - No "use client")
+// import React from 'react';
+// import { notFound } from 'next/navigation';
+// import DashboardLayout from '../../../components/DashboardLayout';
+// import './index.scss';
+// import client from '../../../contentful';
+// import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+// import { BLOCKS } from '@contentful/rich-text-types';
+// import Link from 'next/link';
+// import ContactCard from '../../../components/contactCard';
+// import BlogCard from '../../../components/blogCard';
+
+// app/blog/[slug]/page.js - Add metadata export to the same file
 import React from 'react';
 import { notFound } from 'next/navigation';
 import DashboardLayout from '../../../components/DashboardLayout';
@@ -274,6 +286,86 @@ import { BLOCKS } from '@contentful/rich-text-types';
 import Link from 'next/link';
 import ContactCard from '../../../components/contactCard';
 import BlogCard from '../../../components/blogCard';
+
+// Add this metadata function to the same file as your page component
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  
+  // Fetch the blog post
+  try {
+    const response = await client.getEntries({
+      content_type: 'blogPage',
+      'fields.slug': slug,
+      include: 1
+    });
+    
+    if (response.items.length === 0) {
+      return {
+        title: 'Blog Post Not Found',
+        description: 'The requested blog post could not be found.'
+      };
+    }
+    
+    const blog = response.items[0];
+    
+    // Extract first paragraph for description
+    let description = '';
+    if (blog.fields.body) {
+      const content = blog.fields.body.content;
+      const firstParagraph = content.find(item => 
+        item.nodeType === 'paragraph' && 
+        item.content && 
+        item.content.length > 0 &&
+        item.content[0].value
+      );
+      
+      if (firstParagraph) {
+        description = firstParagraph.content[0].value;
+        description = description.length > 160 
+          ? description.substring(0, 157) + '...' 
+          : description;
+      }
+    }
+    
+    // Make sure the image URL is absolute and includes https:
+    const imageUrl = blog.fields.image?.fields?.file?.url 
+      ? `https:${blog.fields.image.fields.file.url}` 
+      : '';
+    
+    return {
+      title: blog.fields.title,
+      description: description,
+      openGraph: {
+        title: blog.fields.title,
+        description: description,
+        url: `https://www.josephtunde.me/blog/${slug}`,
+        siteName: 'Joseph Tunde',
+        images: imageUrl ? [{ url: imageUrl }] : [],
+        locale: 'en_US',
+        type: 'article',
+        article: {
+          publishedTime: blog.fields.date,
+          tags: [blog.fields.tag]
+        }
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: blog.fields.title,
+        description: description,
+        images: imageUrl ? [imageUrl] : []
+      },
+      alternates: {
+        canonical: `https://www.josephtunde.me/blog/${slug}`
+      }
+    };
+  } catch (error) {
+    console.error('Error generating metadata:', error);
+    return {
+      title: 'Blog',
+      description: 'Read my blog posts on product management, leadership, and more.'
+    };
+  }
+}
 
 
 // 1. Fetch data from Contentful on the server
